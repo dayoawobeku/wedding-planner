51dde58024b824db3b4c864cadd6769cc078afb8
Dayo Awobeku <dawobeku@gmail.com>
2021-08-07 22:12:06 +0100

    small fixes

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -7,25 +7,27 @@ import "./HeroSection.css";
 const HeroSection = () => {
   return (
     <div className="hero--section">
-      <Navbar />
-      <div className="hero--section-texts-parent">
-        <div className="hero--section-texts">
-          <h1>Get Married in Style</h1>
-          <p>Elegance and Continued Excellence to Every Event</p>
-          <Link to="/">
-            <Button text="Plan my wedding" className="hero--section-btn" />
-          </Link>
+      <div className="hero--image">
+        <Navbar />
+        <div className="hero--section-texts-parent">
+          <div className="hero--section-texts">
+            <h1>Get Married in Style</h1>
+            <p>Elegance and Continued Excellence to Every Event</p>
+            <Link to="/">
+              <Button text="Plan my wedding" className="hero--section-btn" />
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <div className="images-loop">
-        <div className="images-loop_child"></div>
-        <div className="images-loop_child"></div>
-        <div className="images-loop_child"></div>
-        <div className="images-loop_child"></div>
-        <div className="images-loop_child"></div>
-        <div className="images-loop_child"></div>
-        <div className="images-loop_child"></div>
+        <div className="images-loop">
+          <div className="images-loop_child"></div>
+          <div className="images-loop_child"></div>
+          <div className="images-loop_child"></div>
+          <div className="images-loop_child"></div>
+          <div className="images-loop_child"></div>
+          <div className="images-loop_child"></div>
+          <div className="images-loop_child"></div>
+        </div>
       </div>
     </div>
   );
